5c9f908a4189ac4fe68094df9ef3106930a0f0e8
Fabian Senn <fsenn@hsr.ch>
2012-02-26 10:31:39 +0100

    Test-Commit für Repository-Einbindung in Redmine

--- ZeLaMoSe/src/zelamose/ZeLaMoSe.java
+++ ZeLaMoSe/src/zelamose/ZeLaMoSe.java
@@ -6,14 +6,14 @@ package zelamose;
 
 /**
  *
- * @author feibl
+ * @author Fabian Senn
  */
 public class ZeLaMoSe {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
 }
