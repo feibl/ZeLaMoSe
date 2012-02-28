f7b6f0388f15d095393569d3299ecf456754916c
Patrick Zenhäusern <pzenhaeu@hsr.ch>
2012-02-28 10:19:07 +0100

    test commit

--- ZeLaMoSe/src/zelamose/ZeLaMoSe.java
+++ ZeLaMoSe/src/zelamose/ZeLaMoSe.java
@@ -14,6 +14,6 @@ public class ZeLaMoSe {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    System.out.println("dfsdf");
+    System.out.println("ddfsdf");
   }
 }
