ae0208f29b626895c2edebbe36a66cd0e81d5f5b
Patrick Zenhäusern <pzenhaeu@hsr.ch>
2012-03-08 22:14:35 +0100

    all uis created

--- ZeLaMoSe/src/view/LobbyJFrame.java
+++ ZeLaMoSe/src/view/LobbyJFrame.java
@@ -25,21 +25,212 @@ public class LobbyJFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txaChat = new javax.swing.JTextArea();
+        lblServerIP = new javax.swing.JLabel();
+        lblServerIPValue = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        txtMessage = new javax.swing.JTextField();
+        btnSend = new javax.swing.JButton();
+        lblOtherPlayerName3 = new javax.swing.JLabel();
+        lblOtherPlayerName1 = new javax.swing.JLabel();
+        lblOtherPlayerName2 = new javax.swing.JLabel();
+        btnExit = new javax.swing.JButton();
+        btnStart = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        lblOwnPlayerName = new javax.swing.JLabel();
+        lblOne = new javax.swing.JLabel();
+        lblTwo = new javax.swing.JLabel();
+        lblThree = new javax.swing.JLabel();
+        lblFour = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
+        jSeparator2 = new javax.swing.JSeparator();
+        jSeparator3 = new javax.swing.JSeparator();
+        pnlClock = new javax.swing.JPanel();
+        lblTimeHere = new javax.swing.JLabel();
+        lblTimeHereValue = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(1026, 710));
+
+        txaChat.setColumns(20);
+        txaChat.setRows(5);
+        jScrollPane1.setViewportView(txaChat);
+
+        lblServerIP.setText("Server-IP:");
+
+        lblServerIPValue.setText("<ServerIPValue>");
+
+        jLabel1.setText("ZeLaMoSe-Tetris - Lobby");
+
+        txtMessage.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtMessageActionPerformed(evt);
+            }
+        });
+
+        btnSend.setText("Send");
+
+        lblOtherPlayerName3.setText("<OtherPlayerName3>");
+
+        lblOtherPlayerName1.setText("<OtherPlayerName1>");
+
+        lblOtherPlayerName2.setText("<OtherPlayerName2>");
+
+        btnExit.setText("Exit");
+        btnExit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnExitActionPerformed(evt);
+            }
+        });
+
+        btnStart.setText("Start Game");
+
+        jLabel2.setText("Players:");
+
+        lblOwnPlayerName.setText("<OwnPlayerName>");
+
+        lblOne.setText("1.");
+
+        lblTwo.setText("2.");
+
+        lblThree.setText("3.");
+
+        lblFour.setText("4.");
+
+        pnlClock.setBorder(javax.swing.BorderFactory.createTitledBorder("Clock"));
+        pnlClock.setPreferredSize(new java.awt.Dimension(136, 136));
+
+        javax.swing.GroupLayout pnlClockLayout = new javax.swing.GroupLayout(pnlClock);
+        pnlClock.setLayout(pnlClockLayout);
+        pnlClockLayout.setHorizontalGroup(
+            pnlClockLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 124, Short.MAX_VALUE)
+        );
+        pnlClockLayout.setVerticalGroup(
+            pnlClockLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 113, Short.MAX_VALUE)
+        );
+
+        lblTimeHere.setText("Time you're here:");
+
+        lblTimeHereValue.setText("<TimeHereValue>");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(txtMessage)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnSend, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 501, Short.MAX_VALUE)
+                        .addComponent(lblServerIP, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lblServerIPValue))
+                    .addComponent(jScrollPane1))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(btnExit, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(btnStart, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 228, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(6, 6, 6)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(lblOne)
+                                    .addComponent(lblTwo)
+                                    .addComponent(lblThree)
+                                    .addComponent(lblFour))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(lblOtherPlayerName2)
+                                    .addComponent(lblOtherPlayerName1)
+                                    .addComponent(lblOtherPlayerName3)
+                                    .addComponent(lblOwnPlayerName)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(11, 11, 11)
+                                .addComponent(jLabel2))
+                            .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jSeparator3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(51, 51, 51)
+                        .addComponent(pnlClock, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(lblTimeHere)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lblTimeHereValue)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblServerIP, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(lblServerIPValue)
+                    .addComponent(jLabel1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(6, 6, 6)
+                        .addComponent(jLabel2)
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lblOwnPlayerName)
+                            .addComponent(lblOne))
+                        .addGap(4, 4, 4)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lblOtherPlayerName1)
+                            .addComponent(lblTwo))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lblOtherPlayerName2)
+                            .addComponent(lblThree))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lblOtherPlayerName3)
+                            .addComponent(lblFour))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(pnlClock, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(32, 32, 32)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lblTimeHere)
+                            .addComponent(lblTimeHereValue))
+                        .addGap(42, 42, 42)
+                        .addComponent(jSeparator3, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnStart, javax.swing.GroupLayout.DEFAULT_SIZE, 320, Short.MAX_VALUE))
+                    .addComponent(jScrollPane1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txtMessage)
+                    .addComponent(btnSend, javax.swing.GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE)
+                    .addComponent(btnExit, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+  private void btnExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExitActionPerformed
+    // TODO add your handling code here:
+  }//GEN-LAST:event_btnExitActionPerformed
+
+  private void txtMessageActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtMessageActionPerformed
+    // TODO add your handling code here:
+  }//GEN-LAST:event_txtMessageActionPerformed
 
   /**
    * @param args the command line arguments
@@ -82,5 +273,29 @@ public class LobbyJFrame extends javax.swing.JFrame {
     });
   }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnExit;
+    private javax.swing.JButton btnSend;
+    private javax.swing.JButton btnStart;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JSeparator jSeparator3;
+    private javax.swing.JLabel lblFour;
+    private javax.swing.JLabel lblOne;
+    private javax.swing.JLabel lblOtherPlayerName1;
+    private javax.swing.JLabel lblOtherPlayerName2;
+    private javax.swing.JLabel lblOtherPlayerName3;
+    private javax.swing.JLabel lblOwnPlayerName;
+    private javax.swing.JLabel lblServerIP;
+    private javax.swing.JLabel lblServerIPValue;
+    private javax.swing.JLabel lblThree;
+    private javax.swing.JLabel lblTimeHere;
+    private javax.swing.JLabel lblTimeHereValue;
+    private javax.swing.JLabel lblTwo;
+    private javax.swing.JPanel pnlClock;
+    private javax.swing.JTextArea txaChat;
+    private javax.swing.JTextField txtMessage;
     // End of variables declaration//GEN-END:variables
 }
--- ZeLaMoSe/src/view/MainJFrame.java
+++ ZeLaMoSe/src/view/MainJFrame.java
@@ -25,17 +25,136 @@ public class MainJFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        pnlSinglePlayer = new javax.swing.JPanel();
+        lblSinglePlayer = new javax.swing.JLabel();
+        lblStartGame = new javax.swing.JLabel();
+        pnlMultiPlayer = new javax.swing.JPanel();
+        lblMultiPlayer = new javax.swing.JLabel();
+        lblCreateGame = new javax.swing.JLabel();
+        lblJoinGame = new javax.swing.JLabel();
+        pnlControl = new javax.swing.JPanel();
+        lblHelp = new javax.swing.JLabel();
+        lblExit = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("~ZeLaMoSe - Tetris~");
+
+        pnlSinglePlayer.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
+
+        lblSinglePlayer.setText("SinglePlayer");
+
+        lblStartGame.setText("Start Game");
+
+        javax.swing.GroupLayout pnlSinglePlayerLayout = new javax.swing.GroupLayout(pnlSinglePlayer);
+        pnlSinglePlayer.setLayout(pnlSinglePlayerLayout);
+        pnlSinglePlayerLayout.setHorizontalGroup(
+            pnlSinglePlayerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlSinglePlayerLayout.createSequentialGroup()
+                .addGap(112, 112, 112)
+                .addGroup(pnlSinglePlayerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lblStartGame)
+                    .addComponent(lblSinglePlayer))
+                .addContainerGap(151, Short.MAX_VALUE))
+        );
+        pnlSinglePlayerLayout.setVerticalGroup(
+            pnlSinglePlayerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlSinglePlayerLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lblSinglePlayer)
+                .addGap(18, 18, 18)
+                .addComponent(lblStartGame)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pnlMultiPlayer.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
+
+        lblMultiPlayer.setText("MultiPlayer");
+
+        lblCreateGame.setText("Create Game");
+
+        lblJoinGame.setText("Join Game");
+
+        javax.swing.GroupLayout pnlMultiPlayerLayout = new javax.swing.GroupLayout(pnlMultiPlayer);
+        pnlMultiPlayer.setLayout(pnlMultiPlayerLayout);
+        pnlMultiPlayerLayout.setHorizontalGroup(
+            pnlMultiPlayerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlMultiPlayerLayout.createSequentialGroup()
+                .addGap(113, 113, 113)
+                .addGroup(pnlMultiPlayerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lblMultiPlayer)
+                    .addComponent(lblJoinGame)
+                    .addComponent(lblCreateGame))
+                .addContainerGap(145, Short.MAX_VALUE))
+        );
+        pnlMultiPlayerLayout.setVerticalGroup(
+            pnlMultiPlayerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlMultiPlayerLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lblMultiPlayer)
+                .addGap(18, 18, 18)
+                .addComponent(lblCreateGame)
+                .addGap(18, 18, 18)
+                .addComponent(lblJoinGame)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pnlControl.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
+
+        lblHelp.setText("Help");
+
+        lblExit.setText("Exit");
+
+        javax.swing.GroupLayout pnlControlLayout = new javax.swing.GroupLayout(pnlControl);
+        pnlControl.setLayout(pnlControlLayout);
+        pnlControlLayout.setHorizontalGroup(
+            pnlControlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlControlLayout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addComponent(lblHelp)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 243, Short.MAX_VALUE)
+                .addComponent(lblExit)
+                .addGap(20, 20, 20))
+        );
+        pnlControlLayout.setVerticalGroup(
+            pnlControlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlControlLayout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addGroup(pnlControlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblHelp)
+                    .addComponent(lblExit))
+                .addContainerGap(32, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(41, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(pnlSinglePlayer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(pnlMultiPlayer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(pnlControl, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(45, 45, 45))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(136, 136, 136)
+                .addComponent(jLabel1)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addComponent(jLabel1)
+                .addGap(28, 28, 28)
+                .addComponent(pnlSinglePlayer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(pnlMultiPlayer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(pnlControl, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(27, Short.MAX_VALUE))
         );
 
         pack();
@@ -82,5 +201,16 @@ public class MainJFrame extends javax.swing.JFrame {
     });
   }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel lblCreateGame;
+    private javax.swing.JLabel lblExit;
+    private javax.swing.JLabel lblHelp;
+    private javax.swing.JLabel lblJoinGame;
+    private javax.swing.JLabel lblMultiPlayer;
+    private javax.swing.JLabel lblSinglePlayer;
+    private javax.swing.JLabel lblStartGame;
+    private javax.swing.JPanel pnlControl;
+    private javax.swing.JPanel pnlMultiPlayer;
+    private javax.swing.JPanel pnlSinglePlayer;
     // End of variables declaration//GEN-END:variables
 }
