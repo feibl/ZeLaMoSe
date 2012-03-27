a03594ae6a430441c0c72b6a7f6562f730b4a37d
Patrick Zenhäusern <pzenhaeu@hsr.ch>
2012-03-27 16:57:21 +0200

    added inputlistener, stepgenerator and stuff with that shit

--- ZeLaMoSe/src/domain/InputSampler.java
+++ ZeLaMoSe/src/domain/InputSampler.java
@@ -4,27 +4,25 @@
  */
 package domain;
 
-import domain.actions.Action;
 import domain.actions.InputEvent;
-import domain.actions.MoveAction;
-import domain.actions.MoveAction.Direction;
+import domain.actions.InputEvent;
 import java.awt.KeyEventDispatcher;
 import java.awt.event.KeyEvent;
-import java.util.LinkedList;
-import java.util.Observable;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  *
  * This class is the "KeyListener" of the GUIs he gets the events and notifys the StepGenerator
  * @author Patrick Zenhäusern
  */
-public class InputSampler extends Observable implements KeyEventDispatcher {
+public class InputSampler implements KeyEventDispatcher {
 
-    private Queue<InputEvent> keyEventQueue;
+    private ConcurrentLinkedQueue<InputEvent> keyEventQueue;
 
     public InputSampler() {
-        keyEventQueue = new LinkedList<InputEvent>();
+        keyEventQueue = new ConcurrentLinkedQueue<InputEvent>();
     }
     
     
@@ -37,6 +35,11 @@ public class InputSampler extends Observable implements KeyEventDispatcher {
         return false;
     }
     
+    public Collection<InputEvent> getAndRemoveAllFromQueue(){
+        Collection<InputEvent> c = new ArrayList<InputEvent>();
+        keyEventQueue.removeAll(c);
+        return c;
+    }
     
     
 }
