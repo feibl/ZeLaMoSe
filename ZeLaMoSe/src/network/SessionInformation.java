bc066c917ab5dcf84f74b896640ac696ef6e051d
Fabian Senn <fsenn@hsr.ch>
2012-04-16 14:00:18 +0200

    SessionInformation implementiert compare

--- ZeLaMoSe/src/network/SessionInformation.java
+++ ZeLaMoSe/src/network/SessionInformation.java
@@ -11,30 +11,46 @@ import java.io.Serializable;
  * @author Fabian Senn <fsenn@hsr.ch>
  */
 public class SessionInformation implements Serializable {
-  private final int id;
-  private final String nickname;
-   
-  public SessionInformation(int id, String nickname) {
-    this.id = id;
-    this.nickname = nickname;
-  }
 
-  /**
-   * @return the nickname
-   */
-  public String getNickname() {
-    return nickname;
-  }
+    private final int id;
+    private final String nickname;
 
-  /**
-   * @return the id
-   */
-  public int getId() {
-    return id;
-  }
+    public SessionInformation(int id, String nickname) {
+        this.id = id;
+        this.nickname = nickname;
+    }
 
-   @Override
-   public String toString() {
-      return "SessionInformation{" + "id=" + id + ", nickname=" + nickname + '}';
-   }
+    /**
+     * @return the nickname
+     */
+    public String getNickname() {
+        return nickname;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "SessionInformation{" + "id=" + id + ", nickname=" + nickname + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        if (!(o instanceof SessionInformation)) {
+            return false; // different class
+        }
+        SessionInformation other = (SessionInformation) o;
+        return nickname.equals(other.nickname) && id == other.id;
+    }
 }
