7e3733c8f46519d54da833a4e0a34dfa997b560b
clam <cyrill.lam@gmail.com>
2012-04-17 14:51:11 +0200

    fixed bug in gameEngine

--- ZeLaMoSe/src/domain/GameEngine.java
+++ ZeLaMoSe/src/domain/GameEngine.java
@@ -30,8 +30,8 @@ public class GameEngine extends GameEngineInterface {
     private int score;
     private Block nextBlock;
     private int blockCounter = 0;
-    private long seed;
     private SimulationController simulationController;
+    private Random randomNewLineGenerator;
 
     public Block getNextBlock() {
         return (Block) nextBlock.clone();
@@ -62,7 +62,7 @@ public class GameEngine extends GameEngineInterface {
         score = 0;
         level = 1;
         totalRemovedLines = 0;
-        this.seed = seed;
+        randomNewLineGenerator = new Random(seed);
     }
 
     public Block getCurrentBlock() {
@@ -386,8 +386,8 @@ public class GameEngine extends GameEngineInterface {
 
     private void createNewLineAction(int numberOfLines) {
         Block[][] garbageLines = new Block[Config.gridWidth][numberOfLines];
-        Random random = new Random(seed);
-        int emptyXPosition = random.nextInt(12);
+        
+        int emptyXPosition = randomNewLineGenerator.nextInt(Config.gridWidth);
         GarbageBlock garbageBlock = new GarbageBlock();
         for (int x = 0; x < Config.gridWidth; ++x) {
             if (x == emptyXPosition) {
