6ef5477ae8262294b6f6634e1288dbfda756cea6
Christian Mollekopf <chrigi_1@fastmail.fm>
2012-05-01 14:37:23 +0200

    Really print stacktrace.

--- ZeLaMoSe/src/domain/TetrisController.java
+++ ZeLaMoSe/src/domain/TetrisController.java
@@ -216,7 +216,7 @@ public class TetrisController extends Observable implements Observer {
             currentStep++;
         } catch (IllegalStateException e) {
             System.out.println("Caught illegal state exception: "+e.getMessage());
-            System.out.println(e.toString());
+            e.printStackTrace();
             handleException(e);
             //We cannot recover from illegal state exceptions, abort game
             abortGame();
