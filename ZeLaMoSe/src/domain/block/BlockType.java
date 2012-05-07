1819f42b0afaf03e6fa7329613103112c48f6685
Patrick Zenhäusern <pzenhaeu@hsr.ch>
2012-05-07 18:07:31 +0200

    correct probability for mirror

--- ZeLaMoSe/src/domain/block/BlockType.java
+++ ZeLaMoSe/src/domain/block/BlockType.java
@@ -15,7 +15,7 @@ import java.util.logging.Logger;
 public enum BlockType {
     I(IBlock.class,100), J(JBlock.class,100), L(LBlock.class,100), 
     O(OBlock.class,100), S(SBlock.class,100), T(TBlock.class,100), 
-    Z(ZBlock.class,100), GARBAGE(GarbageBlock.class,0), MIRROR(MirrorBlock.class,100),  
+    Z(ZBlock.class,100), GARBAGE(GarbageBlock.class,0), MIRROR(MirrorBlock.class,10),  
     DARK(DarkBlock.class,10);
     
     private final Class className;
