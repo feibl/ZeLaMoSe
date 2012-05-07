e5a83bf9c61f80e3e407094cc8ef0c1e9724f305
clam <cyrill.lam@gmail.com>
2012-05-07 08:52:06 +0200

    changed block probability

--- ZeLaMoSe/src/domain/block/BlockType.java
+++ ZeLaMoSe/src/domain/block/BlockType.java
@@ -5,7 +5,6 @@
 package domain.block;
 
 import java.lang.reflect.Constructor;
-import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,8 +15,8 @@ import java.util.logging.Logger;
 public enum BlockType {
     I(IBlock.class,100), J(JBlock.class,100), L(LBlock.class,100), 
     O(OBlock.class,100), S(SBlock.class,100), T(TBlock.class,100), 
-    Z(ZBlock.class,100), GARBAGE(GarbageBlock.class,0), MIRROR(MirrorBlock.class,100),  
-    SHADOW(ShadowBlock.class,100);
+    Z(ZBlock.class,100), GARBAGE(GarbageBlock.class,0), MIRROR(MirrorBlock.class,5),  
+    SHADOW(ShadowBlock.class,5);
     
     private final Class className;
     private final int probability;
