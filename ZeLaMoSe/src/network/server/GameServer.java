3f4a02f08847821f64b1bcfcc077c63fc5b67a71
fsenn <feibl@feibl-8530w.hsr.ch>
2012-09-05 18:26:29 +0200

    StandardCtor gelöscht

--- ZeLaMoSe/src/network/server/GameServer.java
+++ ZeLaMoSe/src/network/server/GameServer.java
@@ -40,7 +40,7 @@ public class GameServer extends UnicastRemoteObject implements GameServerInterfa
     private DiscoveryServer discoveryServer;
 
     public GameServer(String serverName, Registry registry) throws RemoteException, MalformedURLException {
-        this();
+        sessionList = new ArrayList<Session>(MAX_SESSIONS);
         File policy = Config.convertRMI(GameServer.class);
         System.setProperty("java.security.policy", policy.getAbsolutePath());
         if (System.getSecurityManager() == null) {
@@ -48,10 +48,6 @@ public class GameServer extends UnicastRemoteObject implements GameServerInterfa
         }
         registry.rebind(serverName, this);
         discoveryServer = new DiscoveryServer();
-    }
-
-    public GameServer() throws RemoteException {
-        sessionList = new ArrayList<Session>(MAX_SESSIONS);
     }
 
     @Override
